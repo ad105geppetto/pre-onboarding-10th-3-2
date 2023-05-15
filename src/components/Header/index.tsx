0580a18cb8e75c0a7d3b02cfc2bc8d414a6c1276
ad105geppetto <ad105geppetto@gmail.com>
2023-05-15 21:11:16 +0900

    refactor: Separate header components into styles and components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./Header.styles";
+
+const Header = () => {
+  return (
+    <header style={S.headerStyle}>
+      <h1 style={S.titleStyle}>Toodos</h1>
+    </header>
+  );
+};
+
+export default Header;
