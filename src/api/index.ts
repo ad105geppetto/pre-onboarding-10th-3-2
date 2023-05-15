45750efecd704783d30dd49c82d32ca6637c7b8e
ad105geppetto <ad105geppetto@gmail.com>
2023-05-15 22:37:48 +0900

    refactor: Add type to api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Config, Data } from "./index.types";
 
 const baseURL = process.env.REACT_APP_API_URL;
 const token = process.env.REACT_APP_TOKEN;
@@ -13,9 +14,9 @@ const baseInstance = axios.create({
 baseInstance.interceptors.response.use(({ data }) => data);
 
 const apiRequest = {
-  get: (url, request) => baseInstance.get(url, request),
-  delete: (url, request) => baseInstance.delete(url, request),
-  post: (url, data, config) => baseInstance.post(url, data, config)
+  get: (url: string, config?: Config) => baseInstance.get(url, config),
+  delete: (url: string, config?: Config) => baseInstance.delete(url, config),
+  post: (url: string, data: Data, config?: Config) => baseInstance.post(url, data, config)
 };
 
 export default apiRequest;
